e340a964a576a0e4d1cbba68eec1a1b849500276
knnan <28503456+knnan@users.noreply.github.com>
2020-07-09 09:44:58 +0530

    updated bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,8 +11,10 @@ sudo apt install python3-pip
 sudo apt install zsh
 sudo apt install powerline fonts-powerline
 
+#powerlevel9k
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 
+#oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 
@@ -21,17 +23,17 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 #zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions\
 
-
+#Install Menlo for Powerline font
 git clone https://github.com/abertsch/Menlo-for-Powerline.git
 cd Menlo-for-Powerline
 sudo mv "Menlo for Powerline.ttf" /usr/share/fonts
 sudo fc-cache -vf /usr/share/fonts/
 rm -rf Menlo-for-Powerline
 
-
+#install nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 
-
+#install fsearch
 sudo add-apt-repository ppa:christian-boxdoerfer/fsearch-daily\
 sudo apt install fsearch
 
